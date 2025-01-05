cf5b5aec39202740cb19c1c628eae0ca4cc93f2f
BigStar0404 <1964667995@qq.com>
2025-01-05 23:04:12 +0800

    cms_0

---
+++ src/main/java/com/atlyx/cms/CommuManageSysApplication.java
@@ -0,0 +1,13 @@
+package com.atlyx.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuManageSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuManageSysApplication.class, args);
+    }
+
+}
