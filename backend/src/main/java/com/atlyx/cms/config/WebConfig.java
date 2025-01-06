6c1c7c297cc88e28ffe0eff014850d530cbecb16
BigStar0404 <1964667995@qq.com>
2025-01-06 23:37:34 +0800

    cms_1

---
+++ backend/src/main/java/com/atlyx/cms/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.atlyx.cms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // React 默认开发端口
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
