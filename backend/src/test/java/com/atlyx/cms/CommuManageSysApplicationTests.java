f9d1acfa3c09035fb7011a6c98f493fa6379b2cd
BigStar0404 <1964667995@qq.com>
2025-01-06 23:36:36 +0800

    cms_1

---
+++ backend/src/test/java/com/atlyx/cms/CommuManageSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlyx.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuManageSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
